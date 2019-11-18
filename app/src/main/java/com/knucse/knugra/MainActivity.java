7885650692b966e18ff976dfe853192e2e5188ca
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2019-11-18 23:37:54 +0900

    Add firebase integration and sample code

--- app/src/main/java/com/knucse/knugra/MainActivity.java
+++ app/src/main/java/com/knucse/knugra/MainActivity.java
@@ -4,18 +4,24 @@ package com.knucse.knugra;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.knucse.knugra.ui.settings.SettingsActivity;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -24,6 +30,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opencensus.tags.Tag;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,6 +66,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        //firebase test
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> student = new HashMap<>();
+        student.put("fisrt", "Ada");
+        student.put("last", "Lovelace");
+        student.put("born", "1231");
+        db.collection("students").document("2016114704").set(student);
+
+        db.collection("students")
+                .add(student)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("", "DocumentSnapshot added with ID : " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("", "Error adding document", e);
+                    } 
+                });
+
     }
 
     @Override
