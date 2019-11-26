da1cea7df420bc62be687d8d05a53dd1417cbd5e
pmi4202 <pmi4202@naver.com>
2019-11-26 20:18:18 +0900

    for login attribute user, student attribute add

--- app/src/main/java/com/knucse/knugra/PD_package/User_package/Adminstaff.java
+++ app/src/main/java/com/knucse/knugra/PD_package/User_package/Adminstaff.java
@@ -1,4 +1,10 @@
 package com.knucse.knugra.PD_package.User_package;
 
 public class Adminstaff extends User {
+    private String admin_name;
+    private long admin_num;
+
+    public login(){
+
+    }
 }
--- app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/StudentList.java
+++ app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/StudentList.java
@@ -1,4 +1,5 @@
 package com.knucse.knugra.PD_package.User_package.Student_package;
 
 public class StudentList {
+    private int std_totalnum;
 }
