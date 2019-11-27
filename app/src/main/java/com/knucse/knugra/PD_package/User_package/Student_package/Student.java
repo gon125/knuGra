a1c2215689712563f37e2337dd018b988564b500
pmi4202 <pmi4202@naver.com>
2019-11-27 20:05:03 +0900

    student singleton pattern

--- app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/Student.java
+++ app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/Student.java
@@ -10,9 +10,26 @@ public class Student extends User {
     private char std_grade;
     private String std_ph;
     private String std_nation;
-    //객체 생성
+    //sigleton pattern
+    private static Student login_std =null;
 
     //login할 때 정보 같이 가져옴
-    public void login(String std_lname, long std_lschoolnum, String std_lbirth, String std_lmajor, char std_lgrade, String std_lph, String std_lnation){
+    private Student(String std_lid, String std_lpw, boolean std_laccess, String std_lname, long std_lschoolnum, String std_lbirth, String std_lmajor, char std_lgrade, String std_lph, String std_lnation){
+        super(std_lid, std_lpw, std_laccess);
+        this.std_name=std_lname;
+        this.std_schoolnum=std_lschoolnum;
+        this.std_birth=std_lbirth;
+        this.std_major=std_lmajor;
+        this.std_grade=std_lgrade;
+        this.std_ph=std_lph;
+        this.std_nation=std_lnation;
+    }
+
+    public static Student getInstance(String std_lid, String std_lpw, boolean std_laccess, String std_lname, long std_lschoolnum, String std_lbirth, String std_lmajor, char std_lgrade, String std_lph, String std_lnation) {
+        // 객채가 이미 있는경우 새로 생성
+        if (login_std==null) {
+            login_std = new Student(std_lid, std_lpw, std_laccess, std_lname, std_lschoolnum, std_lbirth, std_lmajor, std_lgrade, std_lph, std_lnation);
+        }
+        return login_std;
     }
 }
--- app/src/main/java/com/knucse/knugra/PD_package/User_package/User.java
+++ app/src/main/java/com/knucse/knugra/PD_package/User_package/User.java
@@ -4,10 +4,29 @@ public class User {
     protected String user_id;
     protected String user_pw;
     protected boolean user_access;
-    protected String user_mail;
 
-    public void login(String login_id, String login_pw){
+
+    protected User(String login_id, String login_pw, boolean login_access) {
         this.user_id=login_id;
         this.user_pw=login_pw;
+        this.user_access=login_access;
     }
+    /*
+    //sigleton pattern
+    private static User login_user =null;
+
+    private User(String login_id, String login_pw, boolean login_access){
+        this.user_id=login_id;
+        this.user_pw=login_pw;
+        this.user_access=login_access;
+    }
+
+    public static User getInstance(String login_id, String login_pw, boolean login_access) {
+        // 객채가 이미 있는경우 새로 생성
+        if (login_user==null) {
+            login_user = new User(login_id, login_pw, login_access);
+        }
+        return login_user;
+    }
+    */
 }
