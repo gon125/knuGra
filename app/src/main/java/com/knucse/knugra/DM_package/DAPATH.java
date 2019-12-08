619e23153fe0229f3cd095192c92742a641ca728
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2019-12-08 12:27:37 +0900

    Add DAPATH Path variables to access Firebase data storage.

---
+++ app/src/main/java/com/knucse/knugra/DM_package/DAPATH.java
@@ -0,0 +1,18 @@
+package com.knucse.knugra.DM_package;
+
+public class DAPATH { // data access path
+    public static final String GRADUATION_INFO_LIST = "졸업요건정보목록";
+    public static final String STUDENT_LIST = "학생목록";
+    public static final String COMPUTPER_ABEEK = "심화컴퓨터전공(ABEEK)";
+    public static final String COMPUTPER = "심화컴퓨터전공(일반)";
+    public static final String DOUBLE_MAJOR = "복수전공";
+    public static final String MINER = "부전공";
+    public static final String GLOBAL_SOFTWARE = "글로벌소프트웨어전공";
+    public static final String CONNECTED_CORE_SOFTWARE = "SW연계/융합전공";
+    public static final String TEACHING = "교직이수";
+    public static final String FOREIGN_DOUBLE = "외국인복수학위";
+    public static final String FOREIGN_EXCHANGE = "외국인교환학생";
+
+
+
+}
