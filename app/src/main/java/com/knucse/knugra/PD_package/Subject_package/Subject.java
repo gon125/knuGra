dd1870a93cd6fd4283305d7ff49015161aa6e6f9
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2020-01-16 12:57:11 +0900

    Added Subject / SubjectList Class

---
+++ app/src/main/java/com/knucse/knugra/PD_package/Subject_package/Subject.java
@@ -0,0 +1,7 @@
+package com.knucse.knugra.PD_package.Subject_package;
+
+import java.util.HashMap;
+
+public class Subject extends HashMap<String, String> {
+    
+}
---
+++ app/src/main/java/com/knucse/knugra/PD_package/Subject_package/SubjectList.java
@@ -0,0 +1,7 @@
+package com.knucse.knugra.PD_package.Subject_package;
+
+import java.util.HashMap;
+
+public class SubjectList extends HashMap<String, Subject> {
+
+}
