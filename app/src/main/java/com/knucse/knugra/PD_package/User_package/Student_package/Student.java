0db49c5ad14c42ed7d1a19b4af1fb154a3b14e36
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2020-01-20 12:59:36 +0900

    Update Student.java
    added required / design subject list

--- app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/Student.java
+++ app/src/main/java/com/knucse/knugra/PD_package/User_package/Student_package/Student.java
@@ -1,19 +1,28 @@
 package com.knucse.knugra.PD_package.User_package.Student_package;
 
+import com.knucse.knugra.PD_package.Subject_package.Subject;
+import com.knucse.knugra.PD_package.Subject_package.SubjectList;
 import com.knucse.knugra.PD_package.User_package.UserData;
 
 public class Student extends UserData {
     private StudentBasicInfo studentBasicInfo;
     private StudentCareerList studentCareerList;
+    private SubjectList requiredSubjectList;
+    private SubjectList designSubjectList;
 
     public Student() {
         this.studentCareerList = new StudentCareerList();
         this.studentBasicInfo = new StudentBasicInfo();
+        this.requiredSubjectList = new SubjectList();
+        this.designSubjectList = new SubjectList();
     }
 
-    public Student(StudentCareerList studentCareerList, StudentBasicInfo studentBasicInfo){
+    public Student(StudentCareerList studentCareerList, StudentBasicInfo studentBasicInfo,
+                    SubjectList requiredSubjectList, SubjectList designSubjectList){
         this.studentBasicInfo = studentBasicInfo;
         this.studentCareerList = studentCareerList;
+        this.requiredSubjectList = requiredSubjectList;
+        this.designSubjectList = designSubjectList;
     }
 
     public StudentBasicInfo getStudentBasicInfo() {
@@ -30,5 +39,21 @@ public class Student extends UserData {
 
     public void setStudentCareerList(StudentCareerList studentCareerList) {
         this.studentCareerList = studentCareerList;
+    }
+
+    public SubjectList getRequiredSubjectList() {
+        return requiredSubjectList;
+    }
+
+    public void setRequiredSubjectList(SubjectList requiredSubjectList) {
+        this.requiredSubjectList = requiredSubjectList;
+    }
+
+    public SubjectList getDesignSubjectList() {
+        return designSubjectList;
+    }
+
+    public void setDesignSubjectList(SubjectList designSubjectList) {
+        this.designSubjectList = designSubjectList;
     }
 }
