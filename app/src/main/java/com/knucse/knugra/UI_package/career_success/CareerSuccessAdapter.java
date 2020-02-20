3956c01e20c5ab04b66d85f4163f95be499b9ec4
dnbi <51847115+dnbi@users.noreply.github.com>
2020-02-20 16:40:16 +0900

    Update CareerSuccessAdapter.java, activity_login.xml
    Update CareerSuccessAdapter.java, activity_login.xml

--- app/src/main/java/com/knucse/knugra/UI_package/career_success/CareerSuccessAdapter.java
+++ app/src/main/java/com/knucse/knugra/UI_package/career_success/CareerSuccessAdapter.java
@@ -48,7 +48,7 @@ public class CareerSuccessAdapter extends RecyclerView.Adapter<CareerSuccessAdap
         holder.sc_percent.setText(item.getSc_percent());
         holder.success_prg.setProgress(item.getPrg());
         switch (item.getSc_item()){
-            case "필수과목": case "설계과목": case "SW필수": case "SW교양": //case "창업역량":
+            case "필수과목": case "설계과목": case "SW필수": case "SW교양": //case "창업역량": //case "연계전공":
                 holder.recyclerView_expand.setVisibility(View.VISIBLE);
                 holder.recyclerView_expand.setImageResource((holder.subject_layout.getVisibility() == LinearLayout.GONE) ? R.drawable.ic_expand_more_black_24dp : R.drawable.ic_expand_less_black_24dp);
                 ArrayList<String[]> mSubjectData;
@@ -72,6 +72,10 @@ public class CareerSuccessAdapter extends RecyclerView.Adapter<CareerSuccessAdap
 //                    case "창업역량":
 //                        holder.subject_recycler_name.setText(R.string.startup_subject_complete_status);
 //                        mSubjectData = Graduation_Info_List.getResultStartup();
+//                        break;
+//                    case "연계전공":
+//                        holder.subject_recycler_name.setText(R.string.connected_subject_complete_status);
+//                        mSubjectData = Graduation_Info_List.getResult();
 //                        break;
                     default:
                         mSubjectData = Graduation_Info_List.getResultRequired();
